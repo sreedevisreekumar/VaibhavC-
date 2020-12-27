bef9241fd17f5dff5ae44091ba5d05b076d11d81
Sreekumar <sreekumarnp4u@gmail.com>
2020-12-27 11:31:35 +0000

    initial

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace E06_Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MeetAlien();
+            Console.WriteLine("----------------------");
+            MeetAlien();
+            Console.ReadLine();
+        }
+
+        static void MeetAlien()
+        {
+            Random numberGen = new Random();
+            string name = "X-" + numberGen.Next(10, 9999);
+            int age = numberGen.Next(10, 999);
+            Console.WriteLine("Hello, my name is " + name);
+            Console.WriteLine("I am " + age + "years old");
+
+               
         }
     }
 }
